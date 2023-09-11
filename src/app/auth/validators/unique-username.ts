3c21c8cba597ab580a115a947e91023d2dbe257d
artur-helta <artur.helta@onet.pl>
2023-09-11 16:36:02 +0200

    implement async validator

---
+++ src/app/auth/validators/unique-username.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@angular/core";
+import { AbstractControl, AsyncValidator, ValidationErrors } from "@angular/forms";
+import { Observable, catchError, map, of, switchMap } from "rxjs";
+import { AuthService } from "./auth.service";
+
+@Injectable({
+    providedIn: "root"
+})
+export class UniqueUsername implements AsyncValidator {
+
+    constructor(
+        private authService: AuthService
+    ){}
+
+    validate = (control: AbstractControl): Observable<ValidationErrors | null> =>  {
+        const {value} = control;
+
+        return this.authService.userNameAvailable(value).pipe(
+            switchMap(val => {
+                if (val.available){
+                    return of(null);
+                } else {
+                    return of({ nonUniqueUsername: true})  
+                }
+            }),
+            catchError(err => {
+                if (err.error.username) {
+                    return of({ nonUniqueUsername: true})  
+                }
+                return of({ noConnection: true})     
+            })
+        )  
+    }
+}
