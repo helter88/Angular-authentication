25944c76b92bf3c39b7ab80c6120dc655045a3a7
artur-helta <artur.helta@onet.pl>
2023-09-13 11:56:10 +0200

    put input to share module

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class InputComponent {
 
+  @Input() label!: string;
+  @Input() control!: FormControl;
 }
