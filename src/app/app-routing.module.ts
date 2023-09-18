c1c21b47c0f101b7b380b26148bec03b22cdb91e
artur-helta <artur.helta@onet.pl>
2023-09-18 13:34:17 +0200

    implement canLoad guard to protect lazy loaded module inbox

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthCanLoadGuard } from './auth/auth-can-load.guard';
 
 const routes: Routes = [
   {path: "inbox", 
+  canLoad: [AuthCanLoadGuard],
   loadChildren: () => import("./inbox/inbox.module").then(mod => mod.InboxModule)
   }
 ];
